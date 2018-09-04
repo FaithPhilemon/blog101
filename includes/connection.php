fe7c435a622d33729ec17dbd02cac37af9b91778
Faith Philemon <philemonfaith@gmail.com>
2018-09-04 12:49:51 +0100

    add db name in connection

--- includes/connection.php
+++ includes/connection.php
@@ -2,7 +2,7 @@
     $host = "localhost";
     $user = "root";
     $pass = "";
-    $db   ="";
+    $db   = "blog101";
     $conn = mysqli_connect($host, $user, $pass, $db);
     if(!$conn){
         die("Error in database connection". mysqli_connect_error());
