3c7497945316285b7b2385f0df89b55f68440170
Faith Philemon <philemonfaith@gmail.com>
2018-09-06 11:40:05 +0100

    create bs3 and jq links

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/bootstrap.min.3.css">
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.min.3.js"></script>
     <title></title>
 </head>
 <body>
