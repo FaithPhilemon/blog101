24d4387c3948e3151eb8dcd11085c28838eaa780
Faith Philemon <philemonfaith@gmail.com>
2018-09-17 14:50:34 +0100

    crud cat

--- admin/manage-categories.php
+++ admin/manage-categories.php
@@ -2,7 +2,15 @@
     include ("../includes/functions.php");
     include ("../includes/connection.php");
 
-    $msg_success = $err_empty = "";
+    $msg_success = $err_empty = $msg_delete_success = "";
+
+    if (isset($_GET['delete']) && $_GET['delete'] == "success") {
+        $msg_delete_success = '<div class="alert alert-danger">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                    Category deleted!
+                                </div>';
+    }
+
     if (isset($_POST['btnAdd'])) {
 
         $cat = validate($_POST['txt_cat']);
@@ -51,6 +59,40 @@
                         </div>
                     </div>
                 </form>
+            </div>
+
+            <div class="col-md-6">
+            <?php echo $msg_delete_success ?>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NAME</th>
+                            <th>ACTION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                            $sql = "SELECT * FROM categories";
+                            $result = mysqli_query($conn, $sql);
+                            if ($result) {
+                                if (mysqli_num_rows($result) > 0) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        echo '<tr>
+                                                <td>'.$row['id'].'</td>
+                                                <td>'.$row['name'].'</td>
+                                                <td>
+                                                    <a href="edit.php?id='.$row['id'].'" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-edit"></span> Edit</a>
+                                                    <a href="delete.php?id='.$row['id'].'" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span> Delete</a>
+                                                </td>
+                                            </tr>';
+                                    }
+                                }
+                            }
+                        ?>
+                    </tbody>
+                </table>
+                
             </div>
         </div>
     </div>
